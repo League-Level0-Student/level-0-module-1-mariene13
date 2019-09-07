c8bdf9661207f5aedf20d0e4590348532b83b6e8
ilabstaff <ilabstaff@172.16.116.69>
2019-09-07 10:56:47 -0700

    riddler

--- src/_07_riddler/TheRiddler.java
+++ src/_07_riddler/TheRiddler.java
@@ -1,4 +1,7 @@
 package _07_riddler;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
@@ -9,17 +12,21 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+            int score= 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+            JOptionPane.showMessageDialog(null, "There was a plane crash in which every single person was killed. Yet there were 12 survivors. How?");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+     String 
 		// 5. Otherwise, say "wrong" and tell them the answer
 
 		// 6. Add some more riddles
 
 		// 2. Make a pop up to show the score.
+		JOptionPane.showMessageDialog(null,"your score is + score");
+	}
+
+	
 		
 	}
-}
+
 
